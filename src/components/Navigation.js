3354371cff6afdd2e585761bcaa639cfb35f661b
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2025-02-15 19:28:48 -0800

    Minor Navigation changes

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,7 +8,7 @@ export function Navigation() {
             <nav className="flex items-center p-2 justify-between text-sm text-slate-50">
                 <a
                     href="/"
-                    className="flex text-lg lg:text-base hover:text-slate-300 transition-colors"
+                    className="flex text-xl lg:text-base hover:text-slate-300 transition-colors"
                 >
                     eval.bar
                 </a>
@@ -35,7 +35,7 @@ export function Navigation() {
                             <svg className="w-4 h-4" viewBox="0 0 24 24" fill="none" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                             </svg>
-                            About
+                            <span className="hidden sm:inline">About</span>
                         </button>
                     </li>
                     <li>
@@ -48,7 +48,7 @@ export function Navigation() {
                             <svg height="14" viewBox="0 0 16 16" width="16" fill="currentColor">
                                 <path fillRule="evenodd" d="M4.25 2.5c-1.336 0-2.75 1.164-2.75 3 0 2.15 1.58 4.144 3.365 5.682A20.565 20.565 0 008 13.393a20.561 20.561 0 003.135-2.211C12.92 9.644 14.5 7.65 14.5 5.5c0-1.836-1.414-3-2.75-3-1.373 0-2.609.986-3.029 2.456a.75.75 0 01-1.442 0C6.859 3.486 5.623 2.5 4.25 2.5zM8 14.25l-.345.666-.002-.001-.006-.003-.018-.01a7.643 7.643 0 01-.31-.17 22.075 22.075 0 01-3.434-2.414C2.045 10.731 0 8.35 0 5.5 0 2.836 2.086 1 4.25 1 5.797 1 7.153 1.802 8 3.02 8.847 1.802 10.203 1 11.75 1 13.914 1 16 2.836 16 5.5c0 2.85-2.045 5.231-3.885 6.818a22.08 22.08 0 01-3.744 2.584l-.018.01-.006.003h-.002L8 14.25z"/>
                             </svg>
-                            Donate
+                            <span className="hidden sm:inline">Donate</span>
                         </a>
                     </li>
                 </ul>
