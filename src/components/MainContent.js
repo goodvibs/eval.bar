99773b444907723a7fe2a1b2973a6e59f3098809
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2025-02-25 15:21:22 -0800

    Automatically show sidebar

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -24,7 +24,7 @@ export const parseChesscomGameUrl = (pathname) => {
 };
 
 export function MainContent() {
-    const [isSidebarOpen, setSidebarOpen] = React.useState(false);
+    const [isSidebarOpen, setSidebarOpen] = React.useState(true);
     const [loading, setLoading] = React.useState(false);
     const [error, setError] = React.useState(null);
     const [extensionPromptHidden, setExtensionPromptHidden] = React.useState(true);
