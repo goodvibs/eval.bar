106927800650f9ddbeb9c8c53858feae01b9746d
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2025-02-28 09:47:49 -0800

    Improved toggle

--- src/components/analysis/AnalysisToggle.js
+++ src/components/analysis/AnalysisToggle.js
@@ -12,25 +12,35 @@ export function AnalysisToggle({ disabled, isAnalyzing, handleAnalysisClick }) {
                     disabled={disabled}
                 />
                 <div className={`
-                    relative w-14 h-7 border-2 border-slate-400 rounded-full transition-colors duration-300 ease-in-out
+                    relative flex items-center w-14 h-7 rounded-full transition-colors duration-300 ease-in-out
                     ${isAnalyzing
-                    ? 'bg-slate-400'
-                    : 'bg-transparent'
+                    ? 'bg-green-500'
+                    : 'bg-gray-300'
                 }
                     ${disabled ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'}
                 `}>
                     {/* Toggle handle with icon */}
                     <div className={`
-                        absolute h-4 w-4 top-1 rounded-full transition-all duration-300
+                        absolute h-5 w-5 top-1 rounded-full transition-all duration-300
                         flex items-center justify-center bg-slate-100
                         ${isAnalyzing
-                        ? 'translate-x-8 scale-[1.2]'
+                        ? 'translate-x-8'
                         : 'translate-x-1'
                     }
                         shadow-md
                     `}>
                         <AnalyzeIcon />
                     </div>
+                    {/* Text inside the toggle */}
+                    <span className={`
+                        absolute flex text-xs font-medium transition-all duration-300
+                        ${isAnalyzing
+                        ? 'left-2 text-slate-100'
+                        : 'right-1.5 text-slate-500'
+                    }
+                    `}>
+                        {isAnalyzing ? 'ON' : 'OFF'}
+                    </span>
                 </div>
             </label>
         </div>
