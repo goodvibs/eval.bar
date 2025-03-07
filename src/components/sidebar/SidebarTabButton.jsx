76ba59becfa0bb5cd645c6b5efbef24e3ee85688
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2025-03-07 04:12:15 -0800

    Improved sidebar tab button

--- src/components/sidebar/SidebarTabButton.jsx
+++ src/components/sidebar/SidebarTabButton.jsx
@@ -6,7 +6,7 @@ export function SidebarTabButton({ active, label, onClick }) {
                 flex flex-1 items-center justify-center border-t border-x border-transparent
                 hover:border-slate-600 rounded-t-3xl p-2
                 ${active ? 'bg-slate-600 text-slate-100' : 'text-slate-300'}
-                text-sm font-medium
+                text-normal lg:text-sm font-medium tracking-tighter
             `}
         >
             {label}
