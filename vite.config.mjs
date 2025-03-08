1b91f8d3d531978e8ab636341a8a8ca47df2e156
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2025-03-08 08:55:21 -0800

    React compiler

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,13 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const ReactCompilerConfig = {
+    target: '18' // '17' | '18' | '19'
+}
+
 export default defineConfig(() => {
     return {
         build: {
             outDir: 'build',
         },
         plugins: [
-            react(),
+            react({
+                babel: {
+                    plugins: [
+                        ["babel-plugin-react-compiler", ReactCompilerConfig],
+                    ],
+                },
+            }),
         ],
     };
 });
